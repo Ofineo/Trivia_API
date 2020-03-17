e70060ad81c31ca0d302c912e41dd0ffede7de78
Jordi <ofineo@gmail.com>
2020-03-17 20:21:07 +0000

    built test to search a term

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -84,7 +84,17 @@ class TriviaTestCase(unittest.TestCase):
         
         self.assertFalse(question.id in questions)
 
+     def test_search(self):
 
+        search_term = 'man'
+
+        res = self.client().post(f'/questions/{search_term}')
+        data = json.loads(res.data)
+
+        questions = Question.query.filter(Question.question.ilike(f'%{search_term}%')).count()
+        
+        self.assertEqual(data['totalQuestions'],questions)
+        
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
